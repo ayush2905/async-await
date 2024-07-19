40a6c0c8ea979c126d5aee7a580deb83fb74b024
ayush2905 <ayushagrawal2905@gmail.com>
2024-07-20 02:30:38 +0530

    added extracting-zip

--- async_await/src/bin/extract-zip.rs
+++ async_await/src/bin/extract-zip.rs
@@ -26,7 +26,28 @@ fn real_main() -> i32{
         };
         {
             let comment = file.comment();
-            
+            if !comment.is_empty() {
+                println!("File {} comment {}", i, comment);
+            }
+        }
+        if (*file.name()).ends_with('/'){
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size
+            );
+            if let Some(p) = outpath.parent(){
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
         }
     }
 }
